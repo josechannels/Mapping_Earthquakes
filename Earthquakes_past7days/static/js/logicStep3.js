6d8d7187330bf89075bbaf9d276eb2fe2663950b
josechannels <77767360+josechannels@users.noreply.github.com>
2021-05-02 17:09:46 -0700

    section 13-6-3

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -40,9 +40,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-        style: styleInfo
-        }).addTo(map);
-     });
+    style: styleInfo, 
+    onEachFeature: function(feature,layer) {
+    layer.bindPopup("Magnitude:" + feature.properties.mag + "<br>Location: " + feature.properties.place);  
+    }
+  }).addTo(map);
+});
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
 // to calculate the radius.
@@ -50,7 +53,7 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
@@ -58,6 +61,26 @@ function styleInfo(feature) {
 
   };
 };
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
 
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
